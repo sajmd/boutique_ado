13f847cb8e559af4662efea2561b2c9299242cc9
sajmd <sajidjava90@gmail.com>
2021-04-21 14:57:19 +0000

    added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
